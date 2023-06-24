b3ebc4828b4ac27eb724451db0e98c3bab5b2be9
rachmadyassera <rachmad.yasser.a@gmail.com>
2023-06-24 18:58:40 +0700

    pembuatan main page, membuat opsi pilih paket, video materi sampai menit ke 13.30

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_cleaning_service_ui/color_schema.dart';
 
 class MainPage extends StatefulWidget {
   const MainPage({super.key});
@@ -8,8 +9,110 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
+
+
+  String selectedType = "bersih";
+  String selectedFrequency = "monthly";
+
+  void onChangePaketType(String type) {
+    selectedType = type;
+    setState(() {});
+  }
+
+  // void onChangeFrequency(String type) {
+  //   selectedFrequency = frequency;
+  //   setState(() {});
+  // }
+  
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: deepPurple400,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        title: const Text(
+          'Paket Langganan',
+          style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),
+        ),
+      ),
+      body: Container(
+        width: MediaQuery.of(context).size.width,
+        padding: const EdgeInsets.all(20),
+        decoration: const BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(40),
+            topRight: Radius.circular(40),
+          ),
+        ),
+        child: ListView(
+          children: [
+            const SizedBox(
+              height: 5,
+            ),
+            const Text(
+              'Pilih Paket',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                InkWell(
+                  onTap: () {
+                    onChangePaketType("bersih");
+                  },
+                  child: Column(
+                    children: [
+                      Container(
+                        height: 140,
+                        width: MediaQuery.of(context).size.width * 0.40,
+                        decoration: BoxDecoration(
+                          color: deepPurple50,
+                          borderRadius: const BorderRadius.all(
+                            Radius.circular(20),
+                          ),
+                          image: const DecorationImage(
+                            image: AssetImage('assets/image/img1.png'),
+                          ),
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      const Text(
+                        "Paket Bersih",
+                        style: TextStyle(fontWeight: FontWeight.w600),
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      Container(
+                        height: 30,
+                        width: 30,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: deepPurple50,
+                        ),
+                        child: selectedType == "bersih"
+                            ? Icon(
+                                Icons.check_circle,
+                                color: Colors.pink[400],
+                                size: 30,
+                              )
+                            : Container(),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
