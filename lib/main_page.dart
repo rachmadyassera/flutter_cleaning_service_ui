de0835e4aaaa53f87d832db3bd173285568d741a
rachmadyassera <rachmad.yasser.a@gmail.com>
2023-07-02 10:09:02 +0700

    finishing cs online

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -9,8 +9,6 @@ class MainPage extends StatefulWidget {
 }
 
 class _MainPageState extends State<MainPage> {
-
-
   String selectedType = "bersih";
   String selectedFrequency = "monthly";
 
@@ -23,7 +21,7 @@ class _MainPageState extends State<MainPage> {
     selectedFrequency = frequency;
     setState(() {});
   }
-  
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -203,12 +201,162 @@ class _MainPageState extends State<MainPage> {
                       ),
                     ),
                   ),
+                ),
+                InkWell(
+                  onTap: () {
+                    onChangeFrequency("biweekly");
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 110,
+                    decoration: selectedFrequency == "biweekly"
+                        ? BoxDecoration(
+                            color: pink400,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          )
+                        : BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black.withOpacity(0.3),
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                          ),
+                    child: Center(
+                      child: Text(
+                        '2-Mingguan',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: selectedFrequency == "biweekly"
+                                ? Colors.white
+                                : Colors.black),
+                      ),
+                    ),
+                  ),
+                ),
+                InkWell(
+                  onTap: () {
+                    onChangeFrequency("monthly");
+                  },
+                  child: Container(
+                    height: 50,
+                    width: 110,
+                    decoration: selectedFrequency == "monthly"
+                        ? BoxDecoration(
+                            color: pink400,
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(10),
+                            ),
+                          )
+                        : BoxDecoration(
+                            border: Border.all(
+                              color: Colors.black.withOpacity(0.3),
+                            ),
+                            borderRadius:
+                                const BorderRadius.all(Radius.circular(10)),
+                          ),
+                    child: Center(
+                      child: Text(
+                        'Bulanan',
+                        style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.w500,
+                            color: selectedFrequency == "monthly"
+                                ? Colors.white
+                                : Colors.black),
+                      ),
+                    ),
+                  ),
                 )
               ],
-            )
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            const Text(
+              'Pilih Tambahan',
+              style: TextStyle(fontSize: 16, fontWeight: FontWeight.w600),
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                extraWidget("es", "Dapur", true),
+                extraWidget("belanja", "Memasak", false),
+                extraWidget("tirai", "Tirai Kecil", true),
+              ],
+            ),
+            const SizedBox(
+              height: 20,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                extraWidget("teras", "Teras", true),
+                extraWidget("belanja", "Belanja", false),
+                extraWidget("tirai", "Tirai Besar", true),
+              ],
+            ),
           ],
         ),
       ),
+    );
+  }
+
+  Column extraWidget(String img, String name, bool isSelected) {
+    return Column(
+      children: [
+        Stack(
+          children: [
+            Container(
+              height: 70,
+              width: 70,
+              decoration:
+                  BoxDecoration(shape: BoxShape.circle, color: deepPurple400),
+              child: Container(
+                margin: const EdgeInsets.all(17),
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                    image: AssetImage('assets/image/icons/$img.png'),
+                    fit: BoxFit.contain,
+                  ),
+                ),
+              ),
+            ),
+            Positioned(
+              top: 0,
+              right: 0,
+              child: isSelected == true
+                  ? Container(
+                      height: 30,
+                      width: 30,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                        color: Colors.white,
+                      ),
+                      child: Center(
+                        child: Icon(
+                          Icons.check_circle,
+                          color: pink400,
+                        ),
+                      ),
+                    )
+                  : Container(),
+            ),
+          ],
+        ),
+        const SizedBox(
+          height: 5,
+        ),
+        Text(
+          name,
+          style: const TextStyle(fontWeight: FontWeight.w500),
+        )
+      ],
     );
   }
 }
